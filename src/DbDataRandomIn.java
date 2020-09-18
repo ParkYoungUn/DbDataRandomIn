28559dd402b0b67b68f7499a81d37dfe97b3ae94
ParkYoungUn <youngun45@naver.com>
2020-09-18 16:41:01 +0900

    2020/09/18 16:40

--- src/DbDataRandomIn.java
+++ src/DbDataRandomIn.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -6,12 +8,25 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class DbDataRandomIn extends JFrame {
 
-	private JLabel labelColumn = null;
+	private JLabel labelColumn1 = null;
+	private JLabel labelColumn2 = null;
+	private JLabel labelColumn3 = null;
+	private JLabel labelColumn4 = null;
+	private JLabel labelColumn5 = null;
+	private JLabel labelColumn6 = null;
 
-	private JButton btnInselt = null;
+	private JButton btnInsert = null;
+
+	private JTextField tfColumn1 = null;
+	private JTextField tfColumn2 = null;
+	private JTextField tfColumn3 = null;
+	private JTextField tfColumn4 = null;
+	private JTextField tfColumn5 = null;
+	private JTextField tfColumn6 = null;
 
 
 	public DbDataRandomIn() {
@@ -19,12 +34,50 @@ public class DbDataRandomIn extends JFrame {
 		super("DbDataRandomIn");
 
 		preDbTreatment(); // 각각의 PreparedStatement 생성, 초기화 메소드
-		JPanel panel = new JPanel();
-		add(panel);
 
-		labelColumn = new JLabel("컬럼");
+		JPanel panel = new JPanel();	// 화면에 표시될 패널 생성
 
-		panel.add(labelColumn);
+		// 라벨 레이블 생성
+		labelColumn1 = new JLabel("컬럼1");
+		labelColumn2 = new JLabel("컬럼2");
+		labelColumn3 = new JLabel("컬럼3");
+		labelColumn4 = new JLabel("컬럼4");
+		labelColumn5 = new JLabel("컬럼5");
+		labelColumn6 = new JLabel("컬럼6");
+
+		// 값을 입력받거나 표시할 텍스트필드 생성
+		tfColumn1 = new JTextField(8);
+		tfColumn2 = new JTextField(8);
+		tfColumn3 = new JTextField(8);
+		tfColumn4 = new JTextField(8);
+		tfColumn5 = new JTextField(8);
+		tfColumn6 = new JTextField(8);
+
+
+		// 버튼 레이블 생성
+		btnInsert = new JButton("데이터 입력");
+
+		// 패널에 각각의 레이블과 텍스트필드 추가
+		panel.add(labelColumn1);
+		panel.add(tfColumn1);
+		panel.add(labelColumn2);
+		panel.add(tfColumn2);
+		panel.add(labelColumn3);
+		panel.add(tfColumn3);
+		panel.add(labelColumn4);
+		panel.add(tfColumn4);
+		panel.add(labelColumn5);
+		panel.add(tfColumn5);
+		panel.add(labelColumn6);
+		panel.add(tfColumn6);
+
+		// 패널에 버튼 추가
+		panel.add(btnInsert);
+
+
+		Container c = getContentPane();
+		c.add(panel, BorderLayout.SOUTH);
+
 	}
 
 
